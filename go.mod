4171a4a9cc9dfdff4a3fdcc37dcf868a3eb74c1d
Nova Dwiyanto <novadwynt28@gmail.com>
2024-02-11 05:14:55 +0700

    Done 1.0

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-crud
 
 go 1.22.0
+
+require github.com/go-sql-driver/mysql v1.7.1
--- main.go
+++ main.go
@@ -1,2 +1,33 @@
 package main
 
+import (
+	"go-crud/config"
+	"go-crud/controllers/categorycontroller"
+	"go-crud/controllers/homecontroller"
+	"go-crud/controllers/productcontroller"
+	"log"
+	"net/http"
+)
+
+func main() {
+	config.ConnectDB()
+
+	// Home
+	http.HandleFunc("/", homecontroller.Welcome)
+
+	// Category
+	http.HandleFunc("/categories", categorycontroller.Index)
+	http.HandleFunc("/categories/add", categorycontroller.Add)
+	http.HandleFunc("/categories/edit", categorycontroller.Edit)
+	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+
+	// Product
+	http.HandleFunc("/products", productcontroller.Index)
+	http.HandleFunc("/products/add", productcontroller.Add)
+	http.HandleFunc("/products/detail", productcontroller.Detail)
+	http.HandleFunc("/products/edit", productcontroller.Edit)
+	http.HandleFunc("/products/delete", productcontroller.Delete)
+
+	log.Println("Server running on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
